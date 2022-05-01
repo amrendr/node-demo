306385d2904d6c7665b6994590ae1e3aa3a5fc6b
Amrendra Kumar <amrendr@gmail.com>
2022-04-30 20:26:53 -0400

    feat(demo): Display client info from header

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var clientip = require('../service/clientip');
 
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Welcome' });
+
+  res.set('Cache-Control', 'private, must-revalidate, proxy-revalidate');
+
+  var client_ip = clientip.getClientIP(req);
+  var user_agent = req.get('user-agent');
+  var data = [];
+  
+  data = [
+    { text: 'App Version', value: config.appVersion },
+    { text: 'Client IP', value: client_ip },
+    { text: 'User Agent', value: user_agent }
+  ];
+
+  res.render('index', { title: 'Info', data });
 });
 
 module.exports = router;
