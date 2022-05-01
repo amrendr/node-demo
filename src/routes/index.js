40469dbdf55b7ba6ff26cd1bec743f5005297841
Amrendra Kumar <amrendr@gmail.com>
2022-04-30 20:35:03 -0400

    feat(demo): parse usragent str to show device info

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var clientip = require('../service/clientip');
+const Device = require('device-detector-js');
+const deviceDetector = new Device();
 
 router.get('/', function (req, res, next) {
 
@@ -13,7 +15,8 @@ router.get('/', function (req, res, next) {
   data = [
     { text: 'App Version', value: config.appVersion },
     { text: 'Client IP', value: client_ip },
-    { text: 'User Agent', value: user_agent }
+    { text: 'User Agent', value: user_agent },
+    { text: 'Device', value: JSON.stringify(deviceDetector.parse(user_agent), null, 2)}
   ];
 
   res.render('index', { title: 'Info', data });
